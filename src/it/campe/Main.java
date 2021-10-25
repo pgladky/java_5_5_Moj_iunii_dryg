3bc7f06d8f268304c7ce7c386c5f9c8a347d81b8
pgladky <pgladky3@gmail.com>
2021-10-25 22:02:51 +0200

    Task complete

--- src/it/campe/Main.java
+++ src/it/campe/Main.java
@@ -3,6 +3,10 @@ package it.campe;
 public class Main {
 
     public static void main(String[] args) {
-        //напишите тут ваш код
+        int year = 2021;
+        int age = 22;
+        int x = year - age;
+        System.out.println("Мой друг родился в " + x + " году");
+
     }
 }
